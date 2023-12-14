1f88f9863c0c7452c79f906ef849d455f2853434
vizoss <think220216@gmail.com>
2023-12-14 21:04:17 +0800

    add test case

--- test/user_api_test.go
+++ test/user_api_test.go
@@ -110,7 +110,7 @@ func TestUserRegister(t *testing.T) {
 	}
 }
 
-func TestQueryUserInfo(t *testing.T) {
+func TestQueryUserInfoById(t *testing.T) {
 	uri := "/user"
 	url := fmt.Sprintf("%s%s", getUserApiEndpoint(), uri)
 	contentType := "application/json"
@@ -122,6 +122,79 @@ func TestQueryUserInfo(t *testing.T) {
 	task := httptest.NewHttpTestTask(count, concurrent, func(index, channelIndex int, client http.Client) *httptest.Result {
 		startTime := time.Now().UnixMilli()
 		req, errReq := http.NewRequest("GET", fmt.Sprintf("%s/%d", url, uIds[index%count]), nil)
+		req.Header.Set("Content-Type", contentType)
+		if errReq != nil {
+			duration := time.Now().UnixMilli() - startTime
+			return httptest.NewHttpTestResult(index, -2, 0, duration, errReq)
+		}
+		response, errHttp := client.Do(req)
+		duration := time.Now().UnixMilli() - startTime
+		if errHttp != nil {
+			return httptest.NewHttpTestResult(index, 500, 0, duration, errHttp)
+		} else {
+			if response.StatusCode >= 400 {
+				return httptest.NewHttpTestResult(index, response.StatusCode, 0, duration, errHttp)
+			} else {
+				resBytes, err := io.ReadAll(response.Body)
+				if err != nil {
+					return httptest.NewHttpTestResult(index, 500, 0, duration, errHttp)
+				}
+				registerResp := &dto.RegisterRes{}
+				err = json.Unmarshal(resBytes, registerResp)
+				if err != nil {
+					return httptest.NewHttpTestResult(index, 500, 0, duration, errHttp)
+				} else {
+					return httptest.NewHttpTestResult(index, 200, int64(len(resBytes)), duration, nil)
+				}
+			}
+
+		}
+	}, func(task *httptest.Task) {
+		task.PrintResults()
+		for _, result := range task.Results() {
+			if result.StatusCode() != http.StatusOK {
+				successChan <- false
+				return
+			}
+		}
+		successChan <- true
+		return
+	})
+	task.Start()
+
+	responseCnt := 0
+	responseSuccessCnt := 0
+	for {
+		select {
+		case success, _ := <-successChan:
+			responseCnt++
+			if success {
+				responseSuccessCnt++
+			}
+			if responseCnt == count {
+				if responseCnt == responseSuccessCnt {
+					t.Skip()
+				} else {
+					t.Fail()
+				}
+			}
+			return
+		}
+	}
+}
+
+func TestQueryUserInfoByDisplayId(t *testing.T) {
+	uri := "/user?id="
+	url := fmt.Sprintf("%s%s", getUserApiEndpoint(), uri)
+	contentType := "application/json"
+	count := 1
+	concurrent := 1
+	successChan := make(chan bool)
+	displayIds := make([]string, 0)
+	displayIds = append(displayIds, "1814f3d73e401004")
+	task := httptest.NewHttpTestTask(count, concurrent, func(index, channelIndex int, client http.Client) *httptest.Result {
+		startTime := time.Now().UnixMilli()
+		req, errReq := http.NewRequest("GET", fmt.Sprintf("%s%s", url, displayIds[index%count]), nil)
 		req.Header.Set("Content-Type", contentType)
 		if errReq != nil {
 			duration := time.Now().UnixMilli() - startTime
