aeb57be9f40e5f0a3b08c970861d0c25872b5bbd
vizoss <think220216@gmail.com>
2023-12-17 14:38:28 +0800

    fix bug

--- pkg/sdk/user_token_middleware.go
+++ pkg/sdk/user_token_middleware.go
@@ -20,16 +20,21 @@ func UserTokenAuth(userApi UserApi, logger *logrus.Entry) gin.HandlerFunc {
 		if token == "" {
 			logger.Error("UserTokenAuth: token in header is empty string")
 			baseDto.ResponseForbidden(context)
+			context.Abort()
 			return
 		}
 		req := dto.TokenLoginReq{Token: token, Platform: platform}
 		userInfo, err := userApi.LoginByToken(req)
 		if err != nil {
 			logger.Errorf("UserTokenAuth: %v %v", req, err)
+			baseDto.ResponseForbidden(context)
+			context.Abort()
 			return
 		}
 		if userInfo.User == nil {
 			logger.Errorf("UserTokenAuth: %v %v", req, userInfo)
+			baseDto.ResponseForbidden(context)
+			context.Abort()
 			return
 		}
 		context.Set(UidKey, userInfo.User.Id)
