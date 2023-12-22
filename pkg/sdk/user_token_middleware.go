f107aa7f396b167504d2c83dbf0f4784119cfcaf
vizoss <think220216@gmail.com>
2023-12-22 15:46:34 +0800

    fix bug

--- pkg/sdk/user_token_middleware.go
+++ pkg/sdk/user_token_middleware.go
@@ -5,7 +5,6 @@ import (
 	"github.com/sirupsen/logrus"
 	baseDto "github.com/thk-im/thk-im-base-server/dto"
 	baseMiddleware "github.com/thk-im/thk-im-base-server/middleware"
-	"github.com/thk-im/thk-im-user-server/pkg/dto"
 )
 
 const (
@@ -17,24 +16,15 @@ const (
 func UserTokenAuth(userApi UserApi, logger *logrus.Entry) gin.HandlerFunc {
 	return func(context *gin.Context) {
 		claims := context.MustGet(baseMiddleware.ClaimsKey).(baseDto.ThkClaims)
-		token := context.GetHeader(TokenKey)
-		platform := context.GetHeader(PlatformKey)
-		if token == "" {
-			logger.WithFields(logrus.Fields(claims)).Error("UserTokenAuth: token in header is empty string")
-			baseDto.ResponseForbidden(context)
-			context.Abort()
-			return
-		}
-		req := dto.TokenLoginReq{Token: token, Platform: platform}
-		userInfo, err := userApi.LoginByToken(req, claims)
+		userInfo, err := userApi.LoginByToken(claims)
 		if err != nil {
-			logger.WithFields(logrus.Fields(claims)).Errorf("UserTokenAuth: %v %v", req, err)
+			logger.WithFields(logrus.Fields(claims)).Errorf("UserTokenAuth: %v", err)
 			baseDto.ResponseForbidden(context)
 			context.Abort()
 			return
 		}
 		if userInfo.User == nil {
-			logger.WithFields(logrus.Fields(claims)).Errorf("UserTokenAuth: %v %v", req, userInfo)
+			logger.WithFields(logrus.Fields(claims)).Errorf("UserTokenAuth: %v", userInfo)
 			baseDto.ResponseForbidden(context)
 			context.Abort()
 			return
